420e5f0fad9d37a79c9c21fb32f83095620c2bc6
BhanuPrakash056 <68161637+BhanuPrakash056@users.noreply.github.com>
2022-03-11 16:15:30 +0530

    updated home page

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
 const toggleNavbar = () => {
-  // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
