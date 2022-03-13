12f3d91bd6e24675acffc3a04fa9c5c841d5dcdf
Arunjenson <75872046+Arunjenson@users.noreply.github.com>
2022-03-13 12:50:04 +0530

    commit by arun

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
 const toggleNavbar = () => {
-  // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
+
+const chat = document.querySelector(".chat");
+const bot = document.querySelector(".bot");
+
+function myFunc() {
+  document.getElementsByClassName("chat").style.display = "none"
+  document.getElementsById("botid").style.display = "block";
+  console.log("hi");
+}
+bot.addEventListener("click", myFunc());
