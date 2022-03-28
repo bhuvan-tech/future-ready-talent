c5c1b08ceab7fca6445bbc006de7d7ead10b6343
BhanuPrakash056 <68161637+BhanuPrakash056@users.noreply.github.com>
2022-03-28 17:26:28 +0530

    AREAS

---
+++ ad/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HousingNeedWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HousingNeedWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace HousingNeedWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<HousingNeedWebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("HousingNeedWebAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<HousingNeedWebAppContext>();
+            });
+        }
+    }
+}
